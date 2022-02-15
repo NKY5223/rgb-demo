377c94d1040f6524d64807ef08340cd933cf7ec3
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2022-02-15 16:29:44 +0800

    Update script.js
    changed circle width to 250

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-document.head.innerHTML = `<style>html, body { height: 100%; margin: 0; background: #000000; } .c { mix-blend-mode: screen; width: 100px; height: 100px; border-radius: 50%; position: absolute; cursor: pointer; } .r { background: #ff0000; } .g { background: #00ff00; } .b { background: #0000ff; }</style>`;
+const CIRCLE_SIZE = 250;
+document.head.innerHTML = `<style>html, body { height: 100%; margin: 0; background: #000000; } .c { mix-blend-mode: screen; width: ${CIRCLE_SIZE}px; height: ${CIRCLE_SIZE}px; border-radius: 50%; position: absolute; cursor: pointer; } .r { background: #ff0000; } .g { background: #00ff00; } .b { background: #0000ff; }</style>`;
 const [r, g, b] = new Array(3).fill(0).map(_ => document.createElement("div"));
 Object.entries({ r, g, b }).forEach(([k,e]) => e.classList.add("c", k));
 let dragging = null;
@@ -7,8 +8,8 @@ document.addEventListener("mouseup", _ => { dragging = null });
 document.addEventListener("blur", _ => { dragging = null });
 document.addEventListener("mousemove", e => {
     if (dragging) {
-        dragging.style.left = e.clientX - 50 + "px";
-        dragging.style.top = e.clientY - 50 + "px";
+        dragging.style.left = e.clientX - CIRCLE_SIZE/2 + "px";
+        dragging.style.top = e.clientY - CIRCLE_SIZE/2 + "px";
     }
 });
 [r, g, b].forEach(e => document.body.append(e));
