49cec69bf566545dace0b34a1f157d6291f5c2c8
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2022-02-15 19:47:22 +0800

    making this a not-js-only thing :D

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const CIRCLE_SIZE = 250;
-document.head.innerHTML = `<style>html, body { height: 100%; margin: 0; background: #000000; } .c { mix-blend-mode: screen; width: ${CIRCLE_SIZE}px; height: ${CIRCLE_SIZE}px; border-radius: 50%; position: absolute; cursor: pointer; } .r { background: #ff0000; } .g { background: #00ff00; } .b { background: #0000ff; }</style>`;
-const [r, g, b] = new Array(3).fill(0).map(_ => document.createElement("div"));
-Object.entries({ r, g, b }).forEach(([k,e]) => e.classList.add("c", k));
+let CIRCLE_SIZE = 250;
+
+const r = document.getElementById("r");
+const g = document.getElementById("g");
+const b = document.getElementById("b");
+
+const a = [r, g, b];
 let dragging = null;
-[r, g, b].forEach(e => e.addEventListener("mousedown", _ => { dragging = e }));
-document.addEventListener("mouseup", _ => { dragging = null });
-document.addEventListener("blur", _ => { dragging = null });
+let dragX = 0;
+let dragY = 0;
+
+a.forEach(el => el.addEventListener("mousedown", e => {
+    dragging = el;
+    dragX = e.offsetX;
+    dragY = e.offsetY;
+}));
+document.addEventListener("mouseup", _ => dragging = null);
+document.addEventListener("blur", _ => dragging = null);
 document.addEventListener("mousemove", e => {
     if (dragging) {
-        dragging.style.left = e.clientX - CIRCLE_SIZE/2 + "px";
-        dragging.style.top = e.clientY - CIRCLE_SIZE/2 + "px";
+        dragging.style.left = e.clientX - dragX + "px";
+        dragging.style.top = e.clientY - dragY + "px";
     }
 });
-[r, g, b].forEach(e => document.body.append(e));
